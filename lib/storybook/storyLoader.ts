551e471b70f55e8aabec677d97298e212d253f4f
Yiftachgilboa <yiftachgilboa@gmail.com>
2026-04-07 20:39:38 +0300

    fix: support both anon key env var names

--- lib/storybook/storyLoader.ts
+++ lib/storybook/storyLoader.ts
@@ -4,7 +4,7 @@ import { StoryBook, StoryPage, StoryWithPages } from './types'
 function getClient() {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    (process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY)!
   )
 }
 
